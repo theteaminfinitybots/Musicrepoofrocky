f356119ac624815db7e57de58d0cfac842b4d587
˹ 🎧 ᴛᴇᴀᴍ ɪɴғɪɴɪᴛʏ ʙᴏᴛs ˼ <networksmite@gmail.com>
2026-03-15 16:07:54 +0530

    Update thumbnails.py

--- Spy/utils/thumbnails.py
+++ Spy/utils/thumbnails.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageDraw, ImageEnhance, ImageOps, ImageFilter, ImageFont
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from Spy import app
+from AloneMusic import app
 from config import YOUTUBE_IMG_URL
 
 
